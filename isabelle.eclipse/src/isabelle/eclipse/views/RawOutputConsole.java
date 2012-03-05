f8481e947ab981fa30b6fa9f35b07b8deda21c3b
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-03-05 17:30:02 +0000

    Extracted SessionEventSupport into a generic class that attaches actor listeners to sessions when they become available.

--- isabelle.eclipse/src/isabelle/eclipse/views/RawOutputConsole.java
+++ isabelle.eclipse/src/isabelle/eclipse/views/RawOutputConsole.java
@@ -1,53 +1,41 @@
 package isabelle.eclipse.views;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import java.util.EnumSet;
 import isabelle.Isabelle_Process.Result;
 import isabelle.Session;
 import isabelle.XML;
 import isabelle.eclipse.IsabelleEclipsePlugin;
-import isabelle.eclipse.core.IsabelleCorePlugin;
-import isabelle.eclipse.core.app.IIsabelleSessionListener;
-import isabelle.eclipse.core.app.IIsabelleSystemListener;
-import isabelle.eclipse.core.app.Isabelle;
+import isabelle.eclipse.util.SessionEventSupport;
 import isabelle.scala.ISessionRawMessageListener;
-import isabelle.scala.IsabelleSystemFacade;
 import isabelle.scala.SessionActor;
-import isabelle.scala.SessionFacade;
-
+import isabelle.scala.SessionEventType;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.console.IOConsoleOutputStream;
 import org.eclipse.ui.console.MessageConsole;
 
 public class RawOutputConsole extends MessageConsole {
+	
+	private final SessionEventSupport sessionEvents;
 
-	private final IIsabelleSessionListener appListener;
-	
-	private final Map<SessionFacade, SessionActor> sessionActors = new HashMap<SessionFacade, SessionActor>();
-	
 	private IOConsoleOutputStream consoleStream;
 	
 	public RawOutputConsole(String name, ImageDescriptor imageDescriptor) {
 		super(name, imageDescriptor, true);
 		
-		this.appListener = new IIsabelleSessionListener() {
-
-			@Override
-			public void sessionInit(SessionFacade session) {
-				initSession(session);
-			}
+		sessionEvents = new SessionEventSupport(EnumSet.of(SessionEventType.RAW_MESSAGES)) {
 			
 			@Override
-			public void sessionShutdown(SessionFacade session) {
-				disposeSession(session);
+			protected SessionActor createSessionActor(Session session) {
+				return new SessionActor().rawMessages(new ISessionRawMessageListener() {
+					
+					@Override
+					public void handleMessage(Result result) {
+						outputMessage(result);
+					}
+				});
 			}
-			
 		};
-		
 	}
 
 	@Override
@@ -63,62 +51,12 @@ public class RawOutputConsole extends MessageConsole {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		Isabelle isabelle = IsabelleCorePlugin.getIsabelle();
-		isabelle.addSessionListener(appListener);
-		
-		initSession(isabelle.getSession());
-		
 	}
-	
-	private void initSession(SessionFacade session) {
-		System.out.println("Init session console: " + session);
-		if (session == null) {
-			return;
-		}
-		
-		SessionActor sessionActor = sessionActors.get(session);
-		
-		if (sessionActor == null) {
-			sessionActor = new SessionActor().rawMessages(new ISessionRawMessageListener() {
-				
-				@Override
-				public void handleMessage(ResultFacade result) {
-					outputMessage(result);
-				}
-			});
-			
-			sessionActors.put(session, sessionActor);
-		}
-		
-		session.addRawMessagesActor(sessionActor);
-	}
-	
-	private void disposeSession(SessionFacade session) {
-		
-		if (session == null) {
-			return;
-		}
-		
-		SessionActor sessionActor = sessionActors.get(session);
-		
-		if (sessionActor != null) {
-			session.removeRawMessagesActor(sessionActor);
-			sessionActors.remove(session);
-		}
-	}
-	
 	
 	@Override
 	protected void dispose() {
 		
-		Isabelle isabelle = IsabelleCorePlugin.getIsabelle();
-		isabelle.removeSessionListener(appListener);
-		
-		List<SessionFacade> sessions = new ArrayList<SessionFacade>(sessionActors.keySet());
-		for (SessionFacade session : sessions) {
-			disposeSession(session);
-		}
+		sessionEvents.dispose();
 		
 		if (consoleStream != null) {
 			try {
