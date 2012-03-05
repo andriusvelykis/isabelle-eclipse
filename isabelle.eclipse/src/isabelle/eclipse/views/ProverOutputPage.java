10cf229b4b86278e8543b0dc7199062107e2d60f
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-03-05 17:30:34 +0000

    Prover output update rendering is done in a separate thread, via a Job. Then it is set in the output area in UI thread. To avoid using the UI thread, the editor caret offset is stored after the selection to query from the job, because asking the editor directly requires UI thread.

--- isabelle.eclipse/src/isabelle/eclipse/views/ProverOutputPage.java
+++ isabelle.eclipse/src/isabelle/eclipse/views/ProverOutputPage.java
@@ -4,12 +4,19 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import isabelle.Command;
+import isabelle.Document.Snapshot;
 import isabelle.Markup;
+import isabelle.Session;
 import isabelle.XML.Tree;
+import isabelle.eclipse.IsabelleEclipsePlugin;
+import isabelle.eclipse.editors.DocumentModel;
 import isabelle.eclipse.editors.TheoryEditor;
+import isabelle.scala.SnapshotUtil;
 
 import org.apache.commons.lang.ObjectUtils;
 import org.eclipse.core.runtime.FileLocator;
@@ -17,22 +24,21 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.viewers.IPostSelectionProvider;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.part.IPageSite;
 import org.eclipse.ui.part.Page;
 import org.osgi.framework.Bundle;
 
+import scala.Option;
 
 public class ProverOutputPage extends Page {
 
@@ -49,6 +55,11 @@ public class ProverOutputPage extends Page {
 	
 	private ISelectionChangedListener editorListener;
 	
+	private boolean showTrace = false;
+	private boolean followSelection = true;
+	private int lastEditorCaretOffset = 0;
+	
+	private Job updateJob = new UpdateOutputJob(0, Collections.<Command>emptySet());
 	public ProverOutputPage(TheoryEditor editor) {
 		this.editor = editor;
 	}
@@ -75,7 +86,14 @@ public class ProverOutputPage extends Page {
 
 			@Override
 			public void selectionChanged(SelectionChangedEvent event) {
-				updateOutput();
+				if (followSelection) {
+					// the reaction to selection change is delayed
+//					Session session = IsabelleCorePlugin.getIsabelle().getSession();
+//					long delay = session != null ? session.input_delay().ms() : 300;
+					long delay = 0L;
+					lastEditorCaretOffset = editor.getCaretPosition();
+					updateOutput(delay, lastEditorCaretOffset, Collections.<Command>emptySet());
+				}
 			}
 		};
 		
@@ -88,14 +106,24 @@ public class ProverOutputPage extends Page {
 		
 	}
 	
-	private boolean updateCommand() {
+	private void updateOutput(long delay, int offset, Set<Command> restriction) {
+		Job updateJob = new UpdateOutputJob(offset, restriction);
+		
+		// cancel the previous job
+		this.updateJob.cancel();
+		this.updateJob = updateJob;
+		
+		this.updateJob.schedule(delay);
+	}
+	
+	private boolean updateCommand(int offset) {
 		
 		if (outputArea == null) {
 			// not initialised yet
 			return false;
 		}
 		
-		Command selectedCommand = editor.getSelectedCommand();
+		Command selectedCommand = getCommandAtOffset(offset);
 		if (ObjectUtils.equals(this.currentCommand, selectedCommand)) {
 			return false;
 		}
@@ -103,26 +131,53 @@ public class ProverOutputPage extends Page {
 		this.currentCommand = selectedCommand;
 		return true;
 	}
+	
+	private Command getCommandAtOffset(int offset) {
+		
+		DocumentModel isabelleModel = editor.getIsabelleModel();
+		if (isabelleModel == null) {
+			return null;
+		}
+		
+		Snapshot snapshot = isabelleModel.getSnapshot();
+		
+		Option<Command> cmd = snapshot.node().proper_command_at(offset);
+		return cmd.isDefined() ? cmd.get() : null;
+	}
 
-	private void updateOutput() {
+	private String renderOutput(int offset, Set<Command> restriction, IProgressMonitor monitor) {
 		long start = System.currentTimeMillis();
 		System.out.println("Starting update");
 		// TODO: do not redo the same command?
-		updateCommand();
+		updateCommand(offset);
 		
 		System.out.println("Finished update: " + (System.currentTimeMillis() - start));
 		
 		if (currentCommand == null) {
 			System.out.println("No command");
-			return;
+			return null;
 		}
 		
+		boolean updateCommand = restriction.isEmpty() || restriction.contains(currentCommand);
+		if (!updateCommand) {
+			System.out.println("Ignored command " + currentCommand.name());
+			return null;
+		}
 		
 		// TODO do not output when invisible?
+		// FIXME handle "sendback" in output_dockable.scala
 		
 		// get all command results except tracing
-		List<Tree> commandResults = editor.getSnapshot().commandResults(
-				currentCommand, new String[0], new String[] { Markup.TRACING() });
+		DocumentModel isabelleModel = editor.getIsabelleModel();
+		if (isabelleModel == null) {
+			System.out.println("Isabelle model not available");
+			return null;
+		}
+		
+		Snapshot snapshot = isabelleModel.getSnapshot();
+		String[] excludeFilter = showTrace ? new String[0] : new String[] { Markup.TRACING() };
+		List<Tree> commandResults = SnapshotUtil.getCommandResults(snapshot,
+				currentCommand, new String[0], excludeFilter);
 
 		System.out.println("Got command results: " + (System.currentTimeMillis() - start));
 		
@@ -130,6 +185,7 @@ public class ProverOutputPage extends Page {
 
 		System.out.println("Done rendering: " + (System.currentTimeMillis() - start));
 		
+		return htmlPage;
 	}
 	
 	private List<String> getCssPaths() {
@@ -153,6 +209,19 @@ public class ProverOutputPage extends Page {
 			paths.add(path);
 		} catch (IOException e) {
 			IsabelleEclipsePlugin.log("Unable to locate resource " + pathInBundle, e);
+		}
+	}
+	
+	private void setContent(final String htmlPage) {
+		if (outputArea != null) {
+
+			// set the input in the UI thread
+			outputArea.getDisplay().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					outputArea.setText(htmlPage);
+				}
+			});
 		}
 	}
 	
@@ -187,6 +256,39 @@ public class ProverOutputPage extends Page {
 	
 	public void copySelectionToClipboard() {
 		// do nothing at the moment - allow browser copy facilities to work
+	}
+	
+	private class UpdateOutputJob extends Job {
+
+		private final int offset;
+		private final Set<Command> restriction;
+		
+		public UpdateOutputJob(int offset, Set<Command> restriction) {
+			super("Updating prover output");
+			this.offset = offset;
+			this.restriction = new HashSet<Command>(restriction);
+		}
+
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+			
+			if (monitor.isCanceled()) {
+				return Status.CANCEL_STATUS;
+			}
+			
+			String result = renderOutput(offset, restriction, monitor);
+			
+			// check if cancelled - then do not set the outline
+			if (monitor.isCanceled()) {
+				return Status.CANCEL_STATUS;
+			}
+			
+			if (result != null) {
+				setContent(result);
+			}
+			
+			return Status.OK_STATUS;
+		}
 	}
 
 }
