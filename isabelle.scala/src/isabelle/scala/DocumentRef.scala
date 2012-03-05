efadbea7324b3d68898595571de6c283cba9a2eb
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-03-05 17:30:19 +0000

    Added DocumentRef to encapsulate Document.Node.Name, since the latter cannot be used in Java files directly (due to class nesting Object.Object.Class - the compiler does not like it..)

---
+++ isabelle.scala/src/isabelle/scala/DocumentRef.scala
@@ -0,0 +1,28 @@
+package isabelle.scala
+
+import isabelle._
+
+/**
+ * A wrapper class for Document.Node.Name, since the Document.Node.Name cannot be referenced
+ * in Java (problems with resolving such path - Object.Object.Class).
+ */
+object DocumentRef {
+  
+  def create(path: Path): DocumentRef = new DocumentRef(Document.Node.Name.apply(path))
+  
+  def create(node: String, dir: String, theory: String): DocumentRef = 
+    new DocumentRef(Document.Node.Name(node, dir, theory))
+    
+}
+
+sealed class DocumentRef(ref: Document.Node.Name) {
+ 
+  def getRef() = ref
+  
+  def getNode() = ref.node;
+  
+  def getDir() = ref.dir;
+  
+  def getTheory() = ref.theory;
+  
+}
