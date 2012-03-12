33f0b2e66ae847b4edfd97f8e829e8f3c08cb3fd
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-03-12 20:54:52 +0000

    Exposed session actor exception handling. Note that this changes actor scheduler from the default one used in "= actor {}".

--- isabelle.scala/src/isabelle/scala/SessionActor.scala
+++ isabelle.scala/src/isabelle/scala/SessionActor.scala
@@ -27,34 +27,49 @@ class SessionActor() {
     commandsListener = Some(listener)
     this
   }
+  
+  protected def handleException(e : Exception): Unit = {
+    throw e
+  }
 
-  private val mainActor = actor {
-    loop {
-      react {
-        case phase: Session.Phase => {
-            phaseListener match {
-              case Some(listener) => listener.phaseChanged(phase);
-              case None =>
+  private val mainActor = {
+    val a = new Actor {
+      
+      def act() =
+        loop {
+          react {
+            case phase: Session.Phase => {
+              phaseListener match {
+                case Some(listener) => listener.phaseChanged(phase);
+                case None =>
+              }
             }
+
+            case result: Isabelle_Process.Result => {
+              rawMessageListener match {
+                case Some(listener) => listener.handleMessage(result);
+                case None =>
+              }
+            }
+
+            case changed: Session.Commands_Changed => {
+              commandsListener match {
+                case Some(listener) => listener.commandsChanged(changed);
+                case None =>
+              }
+            }
+
+            case bad => System.err.println("Phase actor: ignoring bad message " + bad)
+          }
         }
 
-        case result: Isabelle_Process.Result => {
-            rawMessageListener match {
-              case Some(listener) => listener.handleMessage(result);
-              case None =>
-            }
-        }
-
-        case changed: Session.Commands_Changed => {
-            commandsListener match {
-              case Some(listener) => listener.commandsChanged(changed);
-              case None =>
-            }
-        }
-
-        case bad => System.err.println("Phase actor: ignoring bad message " + bad)
+      override def exceptionHandler = {
+        case e: Exception => handleException(e)
       }
     }
+    a.start()
+    a
+
   }
 
   private[scala] def getActor() = mainActor
