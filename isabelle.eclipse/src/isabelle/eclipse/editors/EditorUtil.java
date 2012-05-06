1cf09b8d06ab49ffb186635cb40521300cbc7b21
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-05-06 18:57:04 +0100

    Utility methods to reveal a range in editor (adapted/copied from JDT plug-in)

--- isabelle.eclipse/src/isabelle/eclipse/editors/EditorUtil.java
+++ isabelle.eclipse/src/isabelle/eclipse/editors/EditorUtil.java
@@ -1,23 +1,35 @@
 package isabelle.eclipse.editors;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextOperationTarget;
 import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
 import org.eclipse.ui.ide.FileStoreEditorInput;
+import org.eclipse.ui.ide.IGotoMarker;
 import org.eclipse.ui.ide.ResourceUtil;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.IDocumentProvider;
@@ -164,5 +176,100 @@ public class EditorUtil {
 		
 		return null;
 	}
+	
+	/**
+	 * Selects and reveals the given region in the given editor part.
+	 *
+	 * @param part the editor part
+	 * @param element the Java element to reveal
+	 * 
+	 * @see From org.eclipse.jdt.internal.ui.javaeditor.EditorUtility
+	 */
+	public static void revealInEditor(IEditorPart part, IRegion selectRegion, IRegion highlightRegion) {
+		if (part == null || selectRegion == null || highlightRegion == null) {
+			return;
+		}
+		
+		if (part instanceof TheoryEditor) {
+			((TheoryEditor) part).selectInEditor(selectRegion, highlightRegion);
+		} else {
+			revealInEditor(part, selectRegion.getOffset(), selectRegion.getLength(),
+					highlightRegion.getOffset(), highlightRegion.getLength());
+		}
+	}
 
+	/**
+	 * Selects and reveals the given offset and length in the given editor part.
+	 * @param editor the editor part
+	 * @param offset the offset
+	 * @param length the length
+	 * @param highlightOffset the highlight offset
+	 * @param highlightLength the highlight length
+	 * 
+	 * @see From org.eclipse.jdt.internal.ui.javaeditor.EditorUtility
+	 */
+	public static void revealInEditor(IEditorPart editor, final int offset, final int length, 
+			int highlightOffset, int highlightLength) {
+		
+		if (editor instanceof ITextEditor) {
+			((ITextEditor)editor).setHighlightRange(highlightOffset, highlightLength, true);
+			((ITextEditor)editor).selectAndReveal(offset, length);
+			return;
+		}
+
+		// Support for non-text editor - try IGotoMarker interface
+		final IGotoMarker gotoMarkerTarget;
+		if (editor instanceof IGotoMarker)
+			gotoMarkerTarget= (IGotoMarker)editor;
+		else
+			gotoMarkerTarget= editor != null ? (IGotoMarker)editor.getAdapter(IGotoMarker.class) : null;
+		if (gotoMarkerTarget != null) {
+			final IEditorInput input= editor.getEditorInput();
+			if (input instanceof IFileEditorInput) {
+				WorkspaceModifyOperation op = new WorkspaceModifyOperation() {
+					@Override
+					protected void execute(IProgressMonitor monitor) throws CoreException {
+						IMarker marker= null;
+						try {
+							marker= ((IFileEditorInput)input).getFile().createMarker(IMarker.TEXT);
+							marker.setAttribute(IMarker.CHAR_START, offset);
+							marker.setAttribute(IMarker.CHAR_END, offset + length);
+
+							gotoMarkerTarget.gotoMarker(marker);
+
+						} finally {
+							if (marker != null)
+								marker.delete();
+						}
+					}
+				};
+
+				try {
+					op.run(null);
+				} catch (InvocationTargetException ex) {
+					// reveal failed
+				} catch (InterruptedException e) {
+					Assert.isTrue(false, "this operation can not be canceled"); //$NON-NLS-1$
+				}
+			}
+			return;
+		}
+
+		/*
+		 * Workaround: send out a text selection
+		 * XXX: Needs to be improved, see https://bugs.eclipse.org/bugs/show_bug.cgi?id=32214
+		 */
+		if (editor != null && editor.getEditorSite().getSelectionProvider() != null) {
+			IEditorSite site= editor.getEditorSite();
+			if (site == null)
+				return;
+
+			ISelectionProvider provider= editor.getEditorSite().getSelectionProvider();
+			if (provider == null)
+				return;
+
+			provider.setSelection(new TextSelection(offset, length));
+		}
+	}
+	
 }
