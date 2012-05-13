9aeca609df97996d2105324191e3920571e71aa0
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-05-13 21:35:14 +0100

    general JFace interface

--- isabelle.eclipse.ui/src/isabelle/eclipse/ui/editors/ColorManager.java
+++ isabelle.eclipse.ui/src/isabelle/eclipse/ui/editors/ColorManager.java
@@ -3,20 +3,23 @@ package isabelle.eclipse.ui.editors;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.jface.text.source.ISharedTextColors;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 
-public class ColorManager {
+public class ColorManager implements ISharedTextColors {
 
 	protected Map<RGB, Color> fColorTable = new HashMap<RGB, Color>(10);
 
+	@Override
 	public void dispose() {
 		for (Color color : fColorTable.values()) {
 			color.dispose();
 		}
 	}
 	
+	@Override
 	public Color getColor(RGB rgb) {
 		Color color = fColorTable.get(rgb);
 		if (color == null) {
