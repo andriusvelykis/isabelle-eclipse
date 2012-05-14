9cd70d1bff0860ab626312c2e83c3b8cc6b43342
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-05-14 17:53:29 +0100

    rewrote IsabelleTokenScanner as extension of AbstractTokenStreamScanner; updated theory configuration to use all scanners (partition/token/markup) in presentation reconciler

--- isabelle.eclipse.ui/src/isabelle/eclipse/ui/editors/IsabelleTheoryConfiguration.scala
+++ isabelle.eclipse.ui/src/isabelle/eclipse/ui/editors/IsabelleTheoryConfiguration.scala
@@ -1,48 +1,47 @@
 package isabelle.eclipse.ui.editors
 
 import java.util.Map
+import org.eclipse.core.runtime.IAdaptable
+import org.eclipse.jface.text.IDocument
+import org.eclipse.jface.text.TextAttribute
+import org.eclipse.jface.text.contentassist.ContentAssistant
+import org.eclipse.jface.text.contentassist.IContentAssistProcessor
+import org.eclipse.jface.text.contentassist.IContentAssistant
+import org.eclipse.jface.text.presentation.IPresentationReconciler
+import org.eclipse.jface.text.presentation.PresentationReconciler
+import org.eclipse.jface.text.rules.DefaultDamagerRepairer
+import org.eclipse.jface.text.rules.IToken
+import org.eclipse.jface.text.rules.ITokenScanner
+import org.eclipse.jface.text.rules.Token
+import org.eclipse.jface.text.source.ISharedTextColors
+import org.eclipse.jface.text.source.ISourceViewer
+import org.eclipse.swt.graphics.RGB
+import org.eclipse.ui.editors.text.EditorsUI
+import org.eclipse.ui.editors.text.TextSourceViewerConfiguration
+import org.eclipse.ui.texteditor.ChainedPreferenceStore
 
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.TextAttribute;
-import org.eclipse.jface.text.contentassist.ContentAssistant;
-import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
-import org.eclipse.jface.text.contentassist.IContentAssistant;
-import org.eclipse.jface.text.presentation.IPresentationReconciler;
-import org.eclipse.jface.text.presentation.PresentationReconciler;
-import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
-import org.eclipse.jface.text.rules.IToken;
-import org.eclipse.jface.text.rules.Token;
-import org.eclipse.jface.text.source.ISourceViewer;
-import org.eclipse.swt.graphics.RGB;
-import org.eclipse.ui.editors.text.EditorsUI;
-import org.eclipse.ui.editors.text.TextSourceViewerConfiguration;
+import isabelle.Outer_Syntax
+import isabelle.eclipse.ui.IsabelleUIPlugin
+import isabelle.eclipse.ui.text.AbstractIsabelleScanner
+import isabelle.eclipse.ui.text.IsabelleMarkupScanner
+import isabelle.eclipse.ui.text.IsabelleTokenScanner
+import isabelle.eclipse.ui.text.ChainedTokenScanner
+import isabelle.eclipse.ui.text.SingleTokenScanner
+import isabelle.eclipse.ui.preferences.IsabellePartitionToSyntaxClass
+import isabelle.eclipse.ui.preferences.IsabelleMarkupToSyntaxClass
+import isabelle.eclipse.ui.preferences.IsabelleTokenToSyntaxClass
 
-class IsabelleTheoryConfiguration(val editor: TheoryEditor, val colorManager: ColorManager)
-  extends TextSourceViewerConfiguration(EditorsUI.getPreferenceStore()) {
+
+/** @author Andrius Velykis */
+class IsabelleTheoryConfiguration(val editor: TheoryEditor, val colorManager: ISharedTextColors)
+  extends TextSourceViewerConfiguration(new ChainedPreferenceStore(Array(
+      // chain the preference store to get default editor preference values as well as Isabelle-specific
+      IsabelleUIPlugin.getPreferences(),
+      EditorsUI.getPreferenceStore()))) {
 
   /** The hyperlink detector target ID, as defined in plugin.xml */
   val ISABELLE_THEORY_HYPERLINK_TARGET = "isabelle.eclipse.ui.theoryEditor"
   
-  private class IsabelleColorScanner extends IsabelleTokenScanner(editor) {
-
-    override def getToken(tokenInfo: isabelle.Token) =
-      new Token(new TextAttribute(colorManager.getColor(
-        getTokenColor(tokenInfo))))
-  }
-
-  def getTokenColor(tokenInfo: isabelle.Token): RGB = {
-
-    import isabelle.Token.Kind._
-
-    tokenInfo.kind match {
-      case COMMENT => IXMLColorConstants.XML_COMMENT
-      case COMMAND | KEYWORD => IXMLColorConstants.TAG
-      case ALT_STRING | FLOAT | NAT | STRING => IXMLColorConstants.STRING
-      case _ => IXMLColorConstants.DEFAULT
-    }
-  }
-
   override def getConfiguredDocumentPartitioning(sourceViewer: ISourceViewer) = 
     IsabellePartitions.ISABELLE_PARTITIONING
     
@@ -65,20 +64,68 @@ class IsabelleTheoryConfiguration(val editor: TheoryEditor, val colorManager: Co
   override def getPresentationReconciler(sourceViewer: ISourceViewer): IPresentationReconciler = {
     val reconciler = super.getPresentationReconciler(sourceViewer).asInstanceOf[PresentationReconciler]
 
-    def handlePartition(partitionType: String) {
-      val dr = new DefaultDamagerRepairer(new IsabelleColorScanner())
+    /** Sets damager/repairer for the given partition type */
+    def handlePartition(partitionType: String, scanner: Option[ITokenScanner] = None) {
+      
+      // always initialise a scanner for the whole partition
+      val partScanner = partitionScanner(partitionType)
+      
+      // check if another scanner was given - if so, join it on top of the partition scanner
+      val fullScanner = scanner match {
+        case Some(sc) => join (sc, partScanner)
+        case None => partScanner
+      }
+      
+      val dr = new DefaultDamagerRepairer(fullScanner)
       reconciler.setDamager(dr, partitionType)
       reconciler.setRepairer(dr, partitionType)
     }
 
-    handlePartition(IDocument.DEFAULT_CONTENT_TYPE)
-
     // set damager/repairer for each content type
-    IsabellePartitions.contentTypes foreach (
-      contentType => handlePartition(contentType))
+    // add default content type
+    val contentTypes = IDocument.DEFAULT_CONTENT_TYPE :: IsabellePartitions.contentTypes.toList
+    
+    import IsabellePartitions._
+    contentTypes foreach {
+      // for comments, only use the partition scanner - no need to display further scanning
+      case ISABELLE_COMMENT => handlePartition(ISABELLE_COMMENT)
+      // for other content types, use markup & token scanners in addition to partition scanner
+      case contentType => handlePartition(contentType, Some(join(markupScanner(), tokenScanner())))
+    }
 
     reconciler
   }
+  
+  /** Resolve the color manager and preference store values for the abstract trait */
+  private trait IsabelleScanner extends AbstractIsabelleScanner {
+    def colorManager = IsabelleTheoryConfiguration.this.colorManager
+    def preferenceStore = fPreferenceStore
+  }
+
+  /** Joins the scanners in a chained composite scanner */
+  private def join(top: ITokenScanner, bottom: ITokenScanner): ITokenScanner =
+    new ChainedTokenScanner(top, bottom)
+
+  /** Creates a single-token partition scanner which provides tokens for different partition types */
+  private def partitionScanner(partition: String): ITokenScanner =
+    new SingleTokenScanner with IsabelleScanner {
+      override def getToken() =
+        getToken(IsabellePartitionToSyntaxClass(partition))
+    }
+
+  /** Creates a scanner for Isabelle tokens */
+  private def tokenScanner(): ITokenScanner =
+    new IsabelleTokenScanner(editor) with IsabelleScanner {
+      override def getToken(syntax: Outer_Syntax, token: isabelle.Token) =
+        getToken(IsabelleTokenToSyntaxClass(syntax, token))
+    }
+  
+  /** Creates a scanner for Isabelle markup information */
+  private def markupScanner(): ITokenScanner =
+    new IsabelleMarkupScanner(editor) with IsabelleScanner {
+      override def getToken(markupType: String) =
+        getToken(IsabelleMarkupToSyntaxClass(markupType))
+    }
 
   override def getHyperlinkDetectorTargets(sourceViewer: ISourceViewer): Map[String, IAdaptable] = {
 
---
+++ isabelle.eclipse.ui/src/isabelle/eclipse/ui/text/IsabelleTokenScanner.scala
@@ -0,0 +1,77 @@
+package isabelle.eclipse.ui.text
+
+import org.eclipse.jface.text.IDocument
+import org.eclipse.jface.text.rules.IToken
+import org.eclipse.jface.text.rules.Token
+
+import isabelle.Outer_Syntax
+import isabelle.Scan
+import isabelle.Session
+import isabelle.eclipse.ui.editors.TheoryEditor
+
+/** A token scanner that utilises Isabelle/Scala tokeniser. Requires Isabelle Session to be loaded,
+  * which is retrieved via the editor.
+  * 
+  * @author Andrius Velykis
+  */
+class IsabelleTokenScanner(val editor: TheoryEditor) extends AbstractTokenStreamScanner {
+
+  private def syntax: Option[Outer_Syntax] = {
+    val isabelleModel = editor.getIsabelleModel();
+    if (isabelleModel != null && isabelleModel.getSession().is_ready) {
+      Some(isabelleModel.getSession().current_syntax())
+    } else {
+      None
+    }
+  }
+
+  protected def tokenStream(document: IDocument, offset: Int, length: Int): Stream[TokenInfo] =
+    syntax match {
+      case None => {
+        // syntax (Isabelle session) is not available, so cannot tokenise
+        // use stream with undefined token
+        undefinedStream(offset, length)
+      }
+
+      case Some(syntax) => {
+        // use syntax scanner to get Isabelle tokens
+        // TODO do a line-based scanning?
+        val source = document.get(offset, length)
+        val (tokens, _) = syntax.scan_context(source, Scan.Finished)
+        
+        // map the token list as the token stream
+        tokenStream(syntax, tokens, offset)
+      }
+    }
+  
+  /** Constructs a token stream from the token list */
+  private def tokenStream(syntax: Outer_Syntax, tokens: List[isabelle.Token], offset: Int): Stream[TokenInfo] =
+    if (tokens.isEmpty) {
+      Stream.empty
+    } else {
+      
+      // map first token to TokenInfo
+      val token = tokens.head
+      val length = token.source.length();
+      val tokenInfo = TokenInfo(getTokenAll(syntax, token), offset, length)
+      // adjust next token offset
+      val nextOffset = offset + length
+      
+      Stream.cons(tokenInfo, tokenStream(syntax, tokens.tail, nextOffset))
+    }
+  
+  /** Handles SPACE and UNPARSED token kinds as core JFace tokens, delegates everything else to #getToken(Token) */
+  protected def getTokenAll(syntax: Outer_Syntax, token: isabelle.Token): IToken = {
+    import isabelle.Token.Kind._
+    
+    token.kind match {
+      case SPACE => Token.WHITESPACE
+      case UNPARSED => Token.UNDEFINED
+      case _ => getToken(syntax, token)
+    }
+  }
+
+  /** Allow subclasses to specify different JFace tokens for Isabelle tokens, e.g. with colours, etc. */
+  protected def getToken(syntax: Outer_Syntax, token: isabelle.Token): IToken = new Token(token)
+
+}
