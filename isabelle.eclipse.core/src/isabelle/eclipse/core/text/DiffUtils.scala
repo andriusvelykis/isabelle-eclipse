afe693cdd04e3f09c60fee2a249422ada424163e
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-06-10 17:36:27 +0100

    renamed old/new to left/right

--- isabelle.eclipse.core/src/isabelle/eclipse/core/text/DiffUtils.scala
+++ isabelle.eclipse.core/src/isabelle/eclipse/core/text/DiffUtils.scala
@@ -1,7 +1,7 @@
 package isabelle.eclipse.core.text
 
 /** Utilities to perform `diff` on text: find differences between two strings and calculate replacements
-  * to bring the 'old' one to match the 'new'.
+  * to bring the one of the strings to match the other one.
   * 
   * @author Andrius Velykis 
   */
@@ -10,71 +10,69 @@ object DiffUtils {
   /** Finds differences between the given string arguments. Performs word-based matching with additional
     * checks for common word prefix/suffix.
     * 
-    * @param oldText  the 'old' text
-    * @param newText  the 'new' text
-    * @return  list of differences between the texts as (offsetInOld, oldTextDiff, offsetInNew, newTextDiff).
-    *          The differences are based on 'old' text parameters and indicates the offset and
-    *          string to replace to get the 'new' text. 
+    * @param left  first text to compare
+    * @param right  second text to compare
+    * @return  list of differences between the texts as (offsetInLeft, leftTextDiff, offsetInRight, rightTextDiff).
     */
-  def diff(oldText: String, newText: String): List[(Int, String, Int, String)] = {
+  def diff(left: String, right: String): List[(Int, String, Int, String)] = {
 
     import org.eclipse.compare.rangedifferencer.RangeDifferencer
     import StringTokenComparator._
 
     // use word comparators
-    val oldComp = wordComparator(oldText)
-    val newComp = wordComparator(newText)
+    val leftComp = wordComparator(left)
+    val rightComp = wordComparator(right)
 
     // use Eclipse's diff utilities
-    val diffs = RangeDifferencer.findDifferences(oldComp, newComp);
+    val diffs = RangeDifferencer.findDifferences(leftComp, rightComp);
 
     // for each range diff, resolve the actual text diffs
     diffs.toList map { diff =>
-      val (oldOffset, oldContent) = oldComp.tokenRangeContent(diff.leftStart, diff.leftLength)
-      val (newOffset, newContent) = newComp.tokenRangeContent(diff.rightStart, diff.rightLength)
+      val (leftOffset, leftContent) = leftComp.tokenRangeContent(diff.leftStart, diff.leftLength)
+      val (rightOffset, rightContent) = rightComp.tokenRangeContent(diff.rightStart, diff.rightLength)
 
       // words are different - try the difference further by checking for common prefix/suffix
-      val (oldOffsetInWord, oldWordDiff, newOffsetInWord, newWordDiff) = diffWord(oldContent, newContent)
+      val (leftWordOffset, leftWordDiff, rightWordOffset, rightWordDiff) = diffWord(leftContent, rightContent)
       // adjust the word offset
-      (oldOffset + oldOffsetInWord, oldWordDiff, newOffset + newOffsetInWord, newWordDiff)
+      (leftOffset + leftWordOffset, leftWordDiff, rightOffset + rightWordOffset, rightWordDiff)
     }
   }
   
   /** Finds difference between two words. Returns the non-matching middle of the two words by 
     * checking if the words have common prefix or suffix. No further `diff` is performed.
     */
-  def diffWord(oldText: String, newText: String): (Int, String, Int, String) = {
+  def diffWord(left: String, right: String): (Int, String, Int, String) = {
 
     def dropCommonPrefix(diff: (Int, String, Int, String)): (Int, String, Int, String) = {
       // unpack tuple
-      val (oldOffset, oldText, newOffset, newText) = diff
-      val prefix = longestCommonPrefix(oldText, newText)
+      val (leftOffset, left, rightOffset, right) = diff
+      val prefix = longestCommonPrefix(left, right)
       if (prefix > 0) {
         // found common prefix - adjust the offset and drop the prefix
-        (oldOffset + prefix, oldText.substring(prefix), newOffset + prefix, newText.substring(prefix))
+        (leftOffset + prefix, left.substring(prefix), rightOffset + prefix, right.substring(prefix))
       } else {
         // nothing in common - return the originals
-        (oldOffset, oldText, newOffset, newText)
+        (leftOffset, left, rightOffset, right)
       }
     }
 
     def dropCommonSuffix(diff: (Int, String, Int, String)): (Int, String, Int, String) = {
-      val (oldOffset, oldText, newOffset, newText) = diff
-      val suffix = longestCommonPrefix(oldText.reverse, newText.reverse)
+      val (leftOffset, left, rightOffset, right) = diff
+      val suffix = longestCommonPrefix(left.reverse, right.reverse)
       
       def dropRight(str: String, n: Int) = str.substring(0, str.length - n)
 
       if (suffix > 0) {
         // found common suffix - drop it from the strings
-        (oldOffset, dropRight(oldText, suffix), newOffset, dropRight(newText, suffix))
+        (leftOffset, dropRight(left, suffix), rightOffset, dropRight(right, suffix))
       } else {
         // nothing in common - return the originals
-        (oldOffset, oldText, newOffset, newText)
+        (leftOffset, left, rightOffset, right)
       }
     }
 
     // check for common prefix/suffix
-    dropCommonSuffix(dropCommonPrefix(0, oldText, 0, newText))
+    dropCommonSuffix(dropCommonPrefix(0, left, 0, right))
   }
   
   /** Finds the length of the longest common prefix of both arguments. */
