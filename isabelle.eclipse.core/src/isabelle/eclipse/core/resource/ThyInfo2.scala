bdf62ba7aaf8204dc1ff295efa22866029d80397
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-07-09 13:33:41 +0200

    method moved in Isabelle2012

--- isabelle.eclipse.core/src/isabelle/eclipse/core/resource/ThyInfo2.scala
+++ isabelle.eclipse.core/src/isabelle/eclipse/core/resource/ThyInfo2.scala
@@ -2,9 +2,8 @@ package isabelle.eclipse.core.resource
 
 import isabelle.Document
 import isabelle.Path
-import isabelle.Thy_Header
 import isabelle.Thy_Info
-import isabelle.Thy_Load
+
 
 /** An extension of theory information as a workaround for explicitly stating when
   * directory paths are resolved for theory imports. 
@@ -16,7 +15,7 @@ class ThyInfo2(thy_load: ThyLoad2) extends Thy_Info(thy_load) {
   override def import_name(dir: String, str: String): Document.Node.Name =
   {
     val path = Path.explode(str)
-    val node = thy_load.append(dir, Thy_Header.thy_path(path))
+    val node = thy_load.append(dir, thy_load.thy_path(path))
     // use special method to resolve directory path
     val dir1 = thy_load.appendDir(dir, path.dir)
     val theory = path.base.implode
