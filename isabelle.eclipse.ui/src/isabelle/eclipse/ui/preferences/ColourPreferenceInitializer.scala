931a3bf7bffc47bd5f32ee661c2a2fdfcc9e4c13
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-03-07 15:20:45 +0000

    make foreground colour preference optional

--- isabelle.eclipse.ui/src/isabelle/eclipse/ui/preferences/ColourPreferenceInitializer.scala
+++ isabelle.eclipse.ui/src/isabelle/eclipse/ui/preferences/ColourPreferenceInitializer.scala
@@ -1,6 +1,5 @@
 package isabelle.eclipse.ui.preferences
 
-import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer
 import org.eclipse.jface.preference.IPreferenceStore
 import org.eclipse.jface.resource.StringConverter
 import org.eclipse.swt.graphics.RGB
@@ -22,14 +21,15 @@ object ColourPreferenceInitializer {
   
   private def setDefaultsForSyntaxClass(
     syntaxClass: IsabelleSyntaxClass,
-    foreground: RGB,
+    foreground: Option[RGB] = None,
     background: Option[RGB] = None,
     bold: Boolean = false,
     italic: Boolean = false,
     strikethrough: Boolean = false,
     underline: Boolean = false)(implicit prefs: IPreferenceStore) =
     {
-      prefs.setDefault(syntaxClass.foregroundKey, StringConverter.asString(foreground))
+      foreground foreach (fg =>
+        prefs.setDefault(syntaxClass.foregroundKey, StringConverter.asString(fg)))
       background foreach (bg =>
         prefs.setDefault(syntaxClass.backgroundKey, StringConverter.asString(bg)))
       prefs.setDefault(syntaxClass.boldKey, bold)
@@ -40,33 +40,33 @@ object ColourPreferenceInitializer {
 
   private def setDefaultsForSyntaxClasses(implicit prefs: IPreferenceStore) {
     
-    val black = new RGB(0, 0, 0)
-    val red = new RGB(255, 0, 0)
-    val green = new RGB(0, 128, 0)
-    val blue = new RGB(0, 0, 255)
-    val chocolate = new RGB(210, 105, 30)
-    val darkRed = new RGB(139, 0, 0)
-    val keyword1 = new RGB(0, 102, 153)
-    val keyword2 = new RGB(0, 153, 102)
-    val keyword3 = new RGB(43, 157, 255)
-    val quoted = new RGB(240, 240, 240)
+    val black = rgb(0, 0, 0)
+    val red = rgb(255, 0, 0)
+    val green = rgb(0, 128, 0)
+    val blue = rgb(0, 0, 255)
+    val chocolate = rgb(210, 105, 30)
+    val darkRed = rgb(139, 0, 0)
+    val keyword1 = rgb(0, 102, 153)
+    val keyword2 = rgb(0, 153, 102)
+    val keyword3 = rgb(43, 157, 255)
+    val quoted = rgb(240, 240, 240)
     
-    setDefaultsForSyntaxClass(COMMENT, new RGB(204, 0, 0))
-    setDefaultsForSyntaxClass(VERBATIM, black, Some(quoted))
-    setDefaultsForSyntaxClass(STRING, black, Some(quoted))
+    setDefaultsForSyntaxClass(COMMENT, rgb(204, 0, 0))
+    setDefaultsForSyntaxClass(VERBATIM, black, quoted)
+    setDefaultsForSyntaxClass(STRING, black, quoted)
     setDefaultsForSyntaxClass(KEYWORD, keyword1, bold = true)
     setDefaultsForSyntaxClass(KEYWORD2, keyword2, bold = true)
     setDefaultsForSyntaxClass(OPERATOR, black, bold = true)
     setDefaultsForSyntaxClass(LITERAL, keyword1)
     setDefaultsForSyntaxClass(DELIMITER, black)
-    setDefaultsForSyntaxClass(TYPE, new RGB(160, 32, 240))
+    setDefaultsForSyntaxClass(TYPE, rgb(160, 32, 240))
     setDefaultsForSyntaxClass(FREE, blue)
     setDefaultsForSyntaxClass(SKOLEM, chocolate)
     setDefaultsForSyntaxClass(BOUND, green)
-    setDefaultsForSyntaxClass(VAR, new RGB(0, 0, 155))
+    setDefaultsForSyntaxClass(VAR, rgb(0, 0, 155))
     setDefaultsForSyntaxClass(INNER_STRING, chocolate)
     setDefaultsForSyntaxClass(INNER_COMMENT, darkRed)
-    setDefaultsForSyntaxClass(DYN_FACT, new RGB(123, 164, 40))
+    setDefaultsForSyntaxClass(DYN_FACT, rgb(123, 164, 40))
     setDefaultsForSyntaxClass(ANTIQ, blue)
     setDefaultsForSyntaxClass(ML_KEYWORD, keyword1, bold = true)
     setDefaultsForSyntaxClass(ML_NUMERAL, red)
@@ -74,8 +74,10 @@ object ColourPreferenceInitializer {
     setDefaultsForSyntaxClass(ML_COMMENT, darkRed)
     
     setDefaultsForSyntaxClass(CMD, keyword1, bold = true)
-    setDefaultsForSyntaxClass(CMD_SCRIPT, new RGB(246, 52, 36))
+    setDefaultsForSyntaxClass(CMD_SCRIPT, rgb(246, 52, 36))
     setDefaultsForSyntaxClass(CMD_GOAL, keyword3, bold = true)
   }
+  
+  private def rgb(r: Int, g: Int, b: Int): Option[RGB] = Some(new RGB(r, g, b))
 
 }
