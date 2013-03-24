c01f2cefefc933c14640eee9b97b121b5836e00e
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-03-24 21:34:52 +0000

    wrappers for Publisher and ObservableValue

--- isabelle.eclipse.launch/src/isabelle/eclipse/launch/tabs/ObservableUtil.scala
+++ isabelle.eclipse.launch/src/isabelle/eclipse/launch/tabs/ObservableUtil.scala
@@ -22,4 +22,27 @@ object ObservableUtil {
     }
   }
 
+
+  /**
+   * A wrapper for Publisher that adapts the event type.
+   */
+  class AdapterPublisher[Evt, BaseEvt](base: Publisher[BaseEvt])(adapt: BaseEvt => Evt)
+      extends Publisher[Evt] {
+
+    // subscribe to base listener
+    base.subscribe(new base.Sub {
+      override def notify(p: base.Pub, event: BaseEvt) = publish(adapt(event))
+    })
+  }
+
+
+  /**
+   * A wrapper for ObservableValue that adapts the value type.
+   */
+  class AdapterObservableValue[V, BaseV](base: ObservableValue[BaseV])(adapt: BaseV => V)
+      extends AdapterPublisher[V, BaseV](base)(adapt) with ObservableValue[V] {
+    
+    override def value = adapt(base.value)
+  }
+
 }
